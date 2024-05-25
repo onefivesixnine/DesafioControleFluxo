26402818997d760adb0fd1baefd0cae676703537
Thiago s Costa <144514666+onefivesixnine@users.noreply.github.com>
2024-05-24 23:38:00 -0300

    dio

--- Contador.java
+++ Contador.java
@@ -1,24 +1,32 @@
+import java.util.Scanner;
+
 public class Contador {
-	public static void main(String[] args) {
-		Scanner terminal = new Scanner(System.in);
-		System.out.println("Digite o primeiro parâmetro");
-		int parametroUm = terminal.??;
-		System.out.println("Digite o segundo parâmetro");
-		int parametroDois = terminal.??;
-		
-		try {
-			//chamando o método contendo a lógica de contagem
-			contar(parametroUm, parametroDois);
-		
-		}catch (? exception) {
-			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
-		}
-		
-	}
-	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
-		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
-		
-		int contagem = parametroDois - parametroUm;
-		//realizar o for para imprimir os números com base na variável contagem
-	}
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        try {
+            System.out.println("Digite o primeiro parâmetro");
+            int parametroUm = entrada.nextInt();
+
+            System.out.println("Digite o segundo parâmetro");
+            int parametroDois = entrada.nextInt();
+
+            contar(parametroUm, parametroDois);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        } catch (java.util.InputMismatchException e) {
+            System.out.println("Os parâmetros devem ser números.");
+        }
+    }
+
+    public static void contar(int parametroUm, int parametroDois) throws IllegalArgumentException {
+        if (parametroDois <= parametroUm) {
+            throw new IllegalArgumentException("O segundo parâmetro deve ser maior que o primeiro.");
+        }
+        
+        for (int i = parametroUm; i <= parametroDois; i++) {
+            System.out.println(i);
+        }
+    }
 }
